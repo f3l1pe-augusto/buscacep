37be1b2e7f23dafc1478a437de1303f724dd73e9
f3l1pe-augusto <fa.ferreira@unesp.br>
2024-01-09 09:43:19 -0300

    Primeiro commit

---
+++ src/br/com/fakecompany/main/Main.java
@@ -0,0 +1,12 @@
+package br.com.fakecompany.main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digige seu CEP: ");
+        
+    }
+}
