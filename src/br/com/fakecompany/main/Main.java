4946ea4af09eda58ecf1e739a8d7c57c83ce51a3
f3l1pe-augusto <fa.ferreira@unesp.br>
2024-01-09 14:21:47 -0300

    Implementação da requisição HTTP e conexão com a API

--- src/br/com/fakecompany/main/Main.java
+++ src/br/com/fakecompany/main/Main.java
@@ -1,12 +1,41 @@
 package br.com.fakecompany.main;
 
+import br.com.fakecompany.models.EnderecoApi;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digige seu CEP: ");
-        
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+
+        System.out.println("Digite seu CEP: ");
+        int cep = scanner.nextInt();
+
+        String url = "https://viacep.com.br/ws/" + cep + "/json/";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+        System.out.println(json);
+
+        EnderecoApi enderecoApi = gson.fromJson(json, EnderecoApi.class);
+        System.out.println(enderecoApi);
     }
 }
